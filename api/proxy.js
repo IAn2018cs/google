6def39e8f0a2a935073f7baca2b9e885bde5eda9
IAn2018cs <ian2018cs@gmail.com>
2024-07-29 09:49:51 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,14 +2,17 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
   let target = "https://github.com/";//your website url
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
+    if (
+      req.url.startsWith("/api") ||
+      req.url.startsWith("/auth") ||
+      req.url.startsWith("/signup") ||
+      req.url.startsWith("/login")
+    ) {
+      // target = "http://106.15.2.32:6969";
+      res.statusCode = 404;
+      res.end("Not Found");
+      return;
+    }
 
   createProxyMiddleware({
     target,
